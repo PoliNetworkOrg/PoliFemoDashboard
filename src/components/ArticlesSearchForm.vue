840e2c3230834d4845466c40a7585e9b0cf52d06
Francesco Lo Faro <cescolofaro@gmail.com>
2023-10-28 13:25:25 +0200

    .

--- src/components/ArticlesSearchForm.vue
+++ src/components/ArticlesSearchForm.vue
@@ -50,7 +50,7 @@ defineProps({
         :buttons="buttons"
         :formoccurrence="occurrence"
       ></ArticleListItem>
-      <div v-if="articles.length != 0 && canLoadMore" class="mt-4 row">
+      <div v-if="this.articles.length != 0 && canLoadMore" class="mt-4 row">
         <div class="text-primary mx-auto col-auto" role="status">
           <a
             id="loadMore"
